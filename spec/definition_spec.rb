5380d6a1359de0e1b282bc701e5b5e2e7b9d5609
Lake Bramlett <lake.bramlett@gmail.com>
2019-07-26 10:51:51 -0700

    add second definition spec test and logic to pass

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -9,10 +9,18 @@ describe '#Definition' do
 
   describe('.add') do
     it("creates a new definition") do
-      test_def = Definition.new(:body => "colour")
+      test_def = Definition.new(:body => "the quality of an object or substance with respect to light reflected by the object")
       test_def.add
       @definitions = Definition.all
       expect(@definitions).to(eq([test_def]))
+    end
+  end
+
+  describe('.edit') do
+    it("edits a pre-existing definition") do
+      test_def = Definition.new(:body => "the quality of an object or substance with respect to light reflected by the object")
+      test_def.edit(:body => "to cause to appear different from the reality")
+      expect(test_def.body).to(eq("to cause to appear different from the reality"))
     end
   end
 end
